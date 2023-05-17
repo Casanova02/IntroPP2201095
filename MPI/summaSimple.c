47d77add960f333718b438bf0799e58920bf1353
Santiago Leal Casanova <107114168+Casanova02@users.noreply.github.com>
2023-05-17 00:16:39 -0500

    Add files via upload

--- MPI/summaSimple.c
+++ MPI/summaSimple.c
@@ -4,20 +4,36 @@
 // Positive integers 1,2,3...n are known as natural numbers
 
 #include <stdio.h>
+#include <time.h>
 int main()
 {
     int num, count, sum = 0;
+    clock_t start_time, end_time;
+    double total_time;
 
     printf("Enter a positive integer: ");
     scanf("%d", &num);
 
+    start_time = clock();
     // for loop terminates when num is less than count
     for(count = 1; count <= num; ++count)
     {
         sum += count;
     }
 
+
+    end_time = clock();
+
+    total_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
     printf("\nSum = %d\n", sum);
+    printf("Tiempo de ejecucion: %.8f segundos \n", total_time);
+
+
+    FILE *archivo = fopen("output_summaSimpleSerial.txt","w");
+
+    fprintf(archivo,"\n El resultado de la suma es: %d\n",sum);   
+    fprintf(archivo, "Tiempo de ejecucion: %.8f segundos", total_time);
 
     return 0;
 }
